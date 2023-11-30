ac6de94ffbdacecc47047216cb215cb3ecedb90f
Maciegu <nightmarehax@gmail.com>
2023-11-30 20:11:36 +0100

    initial commit

---
+++ actions/api/test.js
@@ -0,0 +1,7 @@
+module.exports = {
+    homepage: function(req, res){
+        res.send('Homepage is working...'); // JEŚLI DZIAŁA TO NA LOCALHOST WYSWIETLA NAM INFORMACJE PODANA W NAWIASIE
+    }
+
+
+}
